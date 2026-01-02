c78648f88bfab2c712d843b457c4d43baa428527
ALEXA8596 <alexjunyoung+github@gmail.com>
2026-01-02 02:20:42 -0800

    Github Pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+  // Add your repository name as the base path
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 };
 
 export default nextConfig;
